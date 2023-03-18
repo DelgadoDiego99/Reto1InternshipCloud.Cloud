4fe40f25a0af8a6f31b82eb7b42c17d7c8f797b4
DelgadoDiego99 <diego.delgado.torres@accenture.com>
2023-03-18 10:14:53 -0500

    Change API path to /api/* since is more accurate

--- CDK/lib/frontend-construct.ts
+++ CDK/lib/frontend-construct.ts
@@ -45,7 +45,7 @@ export class FrontendLayer extends NestedStack {
                 viewerProtocolPolicy: cfn.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
             },
             additionalBehaviors: {
-                "/v1/*": {
+                "/api/*": {
                     origin: new RestApiOrigin(props.apigateway, {
                         originShieldEnabled: true,
                         originShieldRegion: OriginRegionShieldRegion}),
